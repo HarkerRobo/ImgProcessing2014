a83fb24201b0670b5ed97455ddc7db9256234cd8
Manan Shah <manan.shah.777@gmail.com>
2014-05-13 10:18:54 -0700

    Add LineSegment.cs

---
+++ Image Processing/LineSegment.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ImageProcessing2014 {
+	class LineSegment {
+		public int leftX, rightX, Y;
+		public LineSegment(int leftX, int rightX, int Y) {
+			this.leftX = leftX;
+			this.rightX = rightX;
+			this.Y = Y;
+		}
+	}
+}
+
