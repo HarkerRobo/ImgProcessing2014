9fd41d5781349e3d4011ac4e2d20f80766bae3d8
Manan Shah <manan.shah.777@gmail.com>
2014-05-13 10:16:15 -0700

    Add DoublePoint.cs

---
+++ Image Processing/DoublePoint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageProcessing2014 {
+    class DoublePoint {
+        public double X, Y;
+		/// <summary>
+		/// Instead of an Integer Point, this constructor creates a point with double coordinates
+		/// </summary>
+		/// <param name="x">The x coordinate in double format</param>
+		/// <param name="y">The y coordinate in double format</param>
+        public DoublePoint(double x, double y) {
+            X = x;
+            Y = y;
+        }
+    }
+}
